2757834f05d199bade44121675ec3b6524580b5d
Keon Vazir <kvazir@Keons-MacBook-Air.local>
2019-09-12 12:12:11 -0700

    generate route

--- random_word_gen/apps/word_gen_app/urls.py
+++ random_word_gen/apps/word_gen_app/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url
 from . import views
                     
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^random_word$', views.random_word),
 ]
--- random_word_gen/apps/word_gen_app/views.py
+++ random_word_gen/apps/word_gen_app/views.py
@@ -1,8 +1,15 @@
 ######### APP LEVEL views.py ########
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
-def index(request):
+def random_word(request):
     return render(request, "word_gen_app/index.html")
+
+def generate(request):
+    if request.method == "GET":
+        return render(request, "word_gen_app/index.html")
+
+
 
 # Create your views here.
